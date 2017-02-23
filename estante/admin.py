ba5558db444e258a8792d63b6870cdbc4675025e
webeer <brunowber@gmail.com>
2017-02-23 13:48:40 -0300

    Model emprestimo criado

--- estante/admin.py
+++ estante/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from models.pessoa import Pessoa
+from models.emprestimo import Emprestimo
+
 admin.site.register(Pessoa)
+admin.site.register(Emprestimo)
--- estante/models/__init__.py
+++ estante/models/__init__.py
@@ -1,1 +1,4 @@
 from pessoa import Pessoa
+
+from emprestimo import Emprestimo
+
